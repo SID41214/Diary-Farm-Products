09c99e0de5f40c2f3f0087d4b7e6125d644f889f
Aboobacker Sidheeque <sidhub41214@gmail.com>
2024-01-23 12:48:54 +0530

    updated

--- ecart/settings.py
+++ ecart/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'whitenoise.runserver_nostatic',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -154,6 +156,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 # MEDIA_ROOT = BASE_DIR/'media'
 MEDIA_ROOT = BASE_DIR/'uploads'
 MEDIA_URL='/media/'
--- ecart/urls.py
+++ ecart/urls.py
@@ -5,6 +5,9 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.conf import settings # new
+from  django.conf.urls.static import static #new
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,6 +15,9 @@ urlpatterns = [
     # path('',include('customadmin.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
 
 admin.site.site_header="Diary Products"
 admin.site.site_title="Diary Products"
